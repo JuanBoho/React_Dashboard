2f975d2b44851a894f7ebc598dcbb60b6e9d2eb5
JuanBoho <juanbohorquez.ar@gmail.com>
2021-08-17 00:04:59 -0300

    home responsive chart. No final palette

--- src/Content/Home.js
+++ src/Content/Home.js
@@ -1,7 +1,9 @@
-import React, {PureComponent} from 'react';
+import React, {Component, PureComponent} from 'react';
 import classes from './Home.module.css';
 import { LineChart, Line,BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
+const currentHour = new Date().getHours() > 9 ? new Date().getHours() : '0' + new Date().getHours()  ;
+const currentMinutes = new Date().getMinutes() > 9 ? new Date().getMinutes() : '0' + new Date().getMinutes();
 
 const data = [
     {
@@ -34,8 +36,83 @@ const data = [
     },
   ];
 
-const currentHour = new Date().getHours() > 9 ? new Date().getHours() : '0' + new Date().getHours()  ;
-const currentMinutes = new Date().getMinutes() > 9 ? new Date().getMinutes() : '0' + new Date().getMinutes();
+const VentasLineChart = ()=>{
+    
+    return(
+        <ResponsiveContainer width="99%" height={300}>
+            <LineChart                   
+            
+            data={data}
+            margin={{
+            top: 15,
+            right: 20,
+            left: 15,
+            bottom: 5,
+            }}
+        >                  
+            <XAxis 
+                dataKey="name" 
+                axisLine={false} tickLine={false} 
+                padding={{ left: 15 }}
+                style={{fill:'#FDB849'
+                }}
+            />
+            
+            <YAxis
+                domain={['auto', 'auto']}
+                axisLine={false} 
+                tickLine={false}
+                ticks={[0,1000,2000,3000,4000]}
+                style={{fill:'#FDB849'
+                }}
+            />
+
+            <Tooltip 
+                contentStyle={{
+                    backgroundColor:'#000000ea',
+                    border:'none'}}
+                    labelStyle={{color: '#FDB849'
+                }}
+            />
+            
+            <Line 
+                type="monotone" 
+                dataKey="ventas" 
+                stroke="#FDB849" 
+                dot={false}
+                />
+
+        </LineChart>
+        </ResponsiveContainer> );
+}
+
+
+const ProductosBarChart = () =>{
+    return(
+        <ResponsiveContainer width="99%" height={300}>
+            <BarChart  data={data} margin={{
+            top: 15,
+            right: 30,
+            left: 20,
+            bottom: 5,
+            }}>
+            <XAxis dataKey="name" 
+                axisLine={false}
+                tickLine={false}
+                style={{fill:'#FDB849'
+                }}/>
+            <Tooltip 
+                cursor={{
+                    fill:'none'}}
+                contentStyle={{backgroundColor:'#000000ea',
+                               border:'none'}}
+                labelStyle={{color: '#FDB849'}}
+            />
+            <Bar dataKey="ventas" fill="#FDB849"   />
+        </BarChart>
+        </ResponsiveContainer>
+    );
+}
 
 const Home = ()=>{
 
@@ -69,70 +146,15 @@ const Home = ()=>{
             
                 <div className={classes.Homegraph}>
                     <h4>Ventas</h4>
-                
                     <div className={classes.graphVentas}>
-                        <LineChart
-                            width={600}
-                            height={300}
-                            data={data}
-                            margin={{
-                            top: 15,
-                            right: 20,
-                            left: 20,
-                            bottom: 5,
-                            }}
-                        >
-                            
-                            <XAxis 
-                                dataKey="name" 
-                                axisLine={false} tickLine={false} 
-                                padding={{ left: 10 }}
-                            />
-                            
-                            <YAxis
-                                domain={['auto', 'auto']} 
-                                axisLine={false} 
-                                tickLine={false}
-                                ticks={[0,1000,2000,3000,4000]}
-                            />
-
-                            <Tooltip 
-                                contentStyle={{
-                                    backgroundColor:'#000000ea',
-                                    border:'none'}}
-                                    labelStyle={{color: '#FDB849'
-                                }}
-                            />
-                            
-                            <Line 
-                                type="monotone" 
-                                dataKey="ventas" 
-                                stroke="#FDB849" 
-                                dot={false}
-                            />
-
-                        </LineChart>
+                        <VentasLineChart/>
                     </div>
-
                 </div>
             
                 <div className={classes.Homegraph}>
                     <h4>Productos</h4>
                     <div className={classes.graphProductos}>
-                        <BarChart width={390} height={300} data={data} margin={{
-                            top: 15,
-                            right: 30,
-                            left: 20,
-                            bottom: 5,
-                            }}>
-                            <XAxis dataKey="name" axisLine={false} tickLine={false}/>
-                            <Tooltip 
-                                contentStyle={{backgroundColor:'#000000ea',
-                                               border:'none'}}
-                                labelStyle={{color: '#FDB849'}}
-                            />
-                            <Bar dataKey="ventas" fill="#FDB849" />
-                        </BarChart>
+                        <ProductosBarChart/>
                     </div>
                 </div>
 
